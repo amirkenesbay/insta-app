e6e96c64897ec528adc6677860138a19636915d7
amirkenesbay <amirkenesbay@gmail.com>
2022-10-19 23:46:23 +0600

    Made some refactoring in registration controller

--- src/main/java/com/amirkenesbay/sweater/controller/RegistrationController.java
+++ src/main/java/com/amirkenesbay/sweater/controller/RegistrationController.java
@@ -14,24 +14,26 @@ import java.util.Map;
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepo;
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model){
-        User userFromDb = userRepository.findByUsername(user.getUsername());
-        if(userFromDb!=null){
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
             model.put("message", "User exists!");
             return "registration";
         }
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
+        userRepo.save(user);
+
         return "redirect:/login";
     }
 }
