02a96471929ad44026367d08de188135e3db13b3
amirkenesbay <amirkenesbay@gmail.com>
2022-10-19 23:46:42 +0600

    Added ADMIN role for the user

--- src/main/java/com/amirkenesbay/sweater/domain/Role.java
+++ src/main/java/com/amirkenesbay/sweater/domain/Role.java
@@ -3,8 +3,8 @@ package com.amirkenesbay.sweater.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
-
+    USER,
+    ADMIN;
     @Override
     public String getAuthority() {
         return name();
