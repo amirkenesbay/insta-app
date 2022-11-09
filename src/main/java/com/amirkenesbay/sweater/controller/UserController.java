9c01ba5413895718d28a5589ea3ac4ccf3d39da9
amirkenesbay <amirkenesbay@gmail.com>
2022-11-09 19:46:03 +0600

    Refactored user controller class (Changed from user repository to user service)

--- src/main/java/com/amirkenesbay/sweater/controller/UserController.java
+++ src/main/java/com/amirkenesbay/sweater/controller/UserController.java
@@ -2,28 +2,25 @@ package com.amirkenesbay.sweater.controller;
 
 import com.amirkenesbay.sweater.domain.Role;
 import com.amirkenesbay.sweater.domain.User;
-import com.amirkenesbay.sweater.repository.UserRepository;
+import com.amirkenesbay.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    public UserRepository userRepository;
+    public UserService userService;
 
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
@@ -40,22 +37,7 @@ public class UserController {
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user
     ) {
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepository.save(user);
-
+        userService.saveUser(user, username, form);
         return "redirect:/user";
     }
 }
